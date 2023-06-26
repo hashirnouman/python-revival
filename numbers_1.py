8ce6a054bb36fda8ef3ab190cf98a69869d43294
hashir nouman <hashirnouman76@gmail.com>
2023-06-26 08:32:34 +0500

    working on numbers

---
+++ numbers_1.py
@@ -0,0 +1,19 @@
+"""numbers"""
+import math
+
+num = 5
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)  # Gives output in floating number
+print(10 // 3)  # gives output in integer number
+print(10 % 3)  # Modules
+print(10**3)  # Exponential
+
+num += 5  # Augumented assignment operator
+print(num)
+
+print(round(2.6))
+print(abs(-2.9))
+
+print(math.ceil(5.2))
--- string_1.py
+++ string_1.py
@@ -4,3 +4,12 @@ FIRSTNAME = "   Hashir"
 LASTNAME = "nouman  "
 FULLNAME = f" {FIRSTNAME} {LASTNAME}"
 print(FULLNAME.strip())
+print(FULLNAME.rstrip())  # remove white space from end
+print(FULLNAME.lstrip())  # remove white space from start
+print(FULLNAME.upper())
+print(FULLNAME.lower())
+print(FULLNAME.title())  # Capitalize first letter of every word
+print(FULLNAME.find("ir"))  # returns index of the specified string
+print(FULLNAME.replace("ir"))
+print("nou" in FULLNAME)
+print("swfit" not in FULLNAME)  # returns true if swit doesn't exits in FULLNAME
