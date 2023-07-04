983f755d2ffd34b0fd0aac765c909c95ebe96917
hashirnouman <hashirnouman76@gmail.com>
2023-07-04 15:51:00 +0500

    ternary operator complete

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+temprature = 10
+
+# if temprature > 30:
+#     print("ok")
+#     print("good")
+# elif temprature > 12:
+#     print("good")
+# else :
+#     print('nothing')
+# print("done")
+
+# ternary operator
+MESSAGE = "Hot" if temprature >= 20 else "cold"
+print(MESSAGE)
---
+++ type_conversion.py
@@ -0,0 +1,9 @@
+x = input("x: ")
+y = int(x) + 1
+a = 1
+z = str(a)
+print(type(y))
+# falsy values
+#  0
+# ""
+# None
