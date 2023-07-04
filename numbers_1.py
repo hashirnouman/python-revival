8a3d4e4d75605ec2af4959a0fbb4c3d769e80074
hashir nouman <hashirnouman76@gmail.com>
2023-07-04 09:59:44 +0500

    working on numbers

--- numbers_1.py
+++ numbers_1.py
@@ -9,11 +9,8 @@ print(10 / 3)  # Gives output in floating number
 print(10 // 3)  # gives output in integer number
 print(10 % 3)  # Modules
 print(10**3)  # Exponential
-
 num += 5  # Augumented assignment operator
 print(num)
-
 print(round(2.6))
 print(abs(-2.9))
-
 print(math.ceil(5.2))
