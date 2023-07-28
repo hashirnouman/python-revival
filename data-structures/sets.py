aaecaa43b05a310ca0349badd15ca62b0f736fd0
hashir nouman <hashirnouman76@gmail.com>
2023-07-28 21:27:31 +0500

    working with dictioneries

---
+++ data-structures/sets.py
@@ -0,0 +1,35 @@
+"""Sets"""
+# Sets stores unique values. They are unorder.
+# Elements of set cannot be accessed with index like list
+
+# following way to declare a set
+first = {1, 2, 4, 5, 6}
+
+# Following way to covert list to set
+numbers = [1, 1, 3, 3, 4, 7, 8]
+second = set(numbers)
+
+
+# sets have  methods like lists
+second.add(9)
+second.remove(7)
+
+# Union of sets
+print(first | second)
+
+# inrersection of sets
+print(first & second)
+
+# difference between sets
+print(first-second)
+
+# sematic differce
+# returns items in that are in either first or second but not both
+
+print(first ^ second)
+# print(second)
+
+# check exitence of element in set
+
+if 1 in first:
+    print('Yes')
---
+++ data-structures/swapping_variables.py
@@ -0,0 +1,12 @@
+""""Swapping varialbles"""
+# in other languages the following way is followed
+X = 5
+Y = 9
+# Z = X
+# X = Y
+# Y = Z
+# print(X, Y)
+
+# The python way
+X, Y = (Y, X)
+print(X, Y)
