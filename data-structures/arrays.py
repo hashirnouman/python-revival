7f6a2e3bac47ae699be3aee392711203db0fca74
hashir nouman <hashirnouman76@gmail.com>
2023-07-28 21:28:13 +0500

    working with dictioneries

---
+++ data-structures/arrays.py
@@ -0,0 +1,11 @@
+""""Arrays"""
+# Python also provides arrays
+# Arrays are more perfromant that lists
+# Only use arrays when dealing with large series or numbers & encouter performance issues
+# Arrays have similar methods like lists
+
+from array import array
+
+# the first argument is typecode. Typecode shows the data type of array elements
+numbers = array('i', [1, 2, 4, 5, 6])
+print(numbers)
