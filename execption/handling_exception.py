93c8fff623c8a2a2c765cd06022b1c4aec1c47f0
hashir nouman <hashirnouman76@gmail.com>
2023-08-16 16:37:40 +0500

    exception handling completed

--- execption/handling_exception.py
+++ execption/handling_exception.py
@@ -1,4 +1,6 @@
 """ Exception handling"""
+# timeit package it used to calculate excecution time of piece of code
+from timeit import timeit
 # try:
 #     age = int(input('Enter age  '))
 #     print(age)
@@ -58,3 +60,44 @@ except (ValueError, ZeroDivisionError) as err:
     print(type(err))
 else:
     print('No error')
+file.close()
+
+# raising an execption (not advised) because it is costly
+
+CODE1 = """
+def calculate_x_factor(total_age):
+    if total_age <= 0:
+        raise ValueError('Age cannot be 0 or less')
+    else:
+        print(10/total_age)
+
+
+# when you raise exception in a function
+# you've to call it with try catch else it will throw error
+
+try:
+    calculate_x_factor(0)
+except ValueError as error:
+    pass
+"""
+# this approach is faster
+CODE2 = """
+def calculate_x_factor(total_age):
+    if total_age <= 0:
+        return None
+    else:
+        print(10/total_age)
+
+
+# when you raise exception in a function
+# you've to call it with try catch else it will throw error
+
+
+result = calculate_x_factor(0)
+if result == None:
+    pass
+"""
+# number is number of execution. This should be greater to see the time difference
+print(timeit(CODE1, number=10000))
+print(timeit(CODE2, number=10000))
+# cost of raising an exception
