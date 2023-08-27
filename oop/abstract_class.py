b80d82f9c61e1a72250d04523aa3591549e2d805
hashirnouman <hashirnouman76@gmail.com>
2023-08-27 19:49:39 +0500

    worked on abstract classes

---
+++ oop/abstract_class.py
@@ -0,0 +1,53 @@
+"""Abstract class"""
+
+# abstract base class is a blueprint for other classes
+# it means that the other classes are derieved from it but it is new initialized
+# if a class is used as blueprint of other class but it is not abstract base class
+# then this class can be initiated. To prevent this abstract class is declared
+
+from abc import ABC, abstractmethod
+
+
+class InvalidOperatiornError(Exception):
+    """Extending exception class"""
+
+
+class Stream(ABC):
+    """Stream base abstract class"""
+
+    def __init__(self):
+        self.open = False
+
+    def open_stream(self):
+        """open method"""
+        if self.open:
+            raise InvalidOperatiornError('Stream Already open')
+
+    def close_stream(self):
+        """open method"""
+        if self.open:
+            raise InvalidOperatiornError('Stream Already open')
+
+    @abstractmethod
+    def read(self):
+        """read abstract method"""
+
+
+class FileStream(Stream):
+    """file stream class"""
+
+    def read(self):
+        """read method"""
+        print('Reading from file stream')
+
+
+class NetWorkStream(Stream):
+    """file stream class"""
+
+    def read(self):
+        """read method"""
+        print('Reading from network stream')
+
+
+n = NetWorkStream()
+n.read()
--- oop/multiple_inheritence.py
+++ oop/multiple_inheritence.py
@@ -21,9 +21,37 @@ class Employee:
         print('hello from employee')
 
 
+# in this code first it will look for greet function in first base class Person then in Employee
+# if we declare Employee first and Person second it will change the behaviour
 class Manager(Person, Employee):
     """Manager class"""
 
 
 m = Manager()
 m.greet()
+
+# a good example
+
+
+class Flyer:
+    """Flyer class"""
+
+    def fly(self):
+        """fly """
+        print('fly')
+
+
+class Swimmer:
+    """Swimmer class class"""
+
+    def swim(self):
+        """swim """
+        print('swim')
+
+
+class Duck(Flyer, Swimmer):
+    """Duck class"""
+
+
+d = Duck()
+d.fly()
