e0b1835f0e24958014060491bc3d36bfc549125b
hashirnouman <hashirnouman76@gmail.com>
2023-09-17 18:29:39 +0500

    working on files

--- python-statndard-libraries/directories.py
+++ python-statndard-libraries/directories.py
@@ -1,6 +1,7 @@
 """Directories"""
 from pathlib import Path
-
-path = Path('modules/ecom')
-print(path.rename('modules/ecommerce'))
- 
+from time import ctime
+path = Path('modules/ecom/shopping/sales.py')
+print(ctime(path.stat().st_ctime))
+print(path.read_text(encoding='UTF-8'))
+print(path.write_text("print('hello')", encoding='UTF-8'))
--- python-statndard-libraries/paths.py
+++ python-statndard-libraries/paths.py
@@ -2,8 +2,11 @@
 from pathlib import Path
 
 Path(r'C:\Program Files\user')
-path = Path('modules/ecom/shopping/sales.py')
+path = Path('ecommerce')
 print(path.exists())
 print(path.is_file())
 print(path.parent)
 print(Path.home())
+print(path.iterdir())
+p = [p for p in path.iterdir()]
+print(p)
